188237602cb82c39b58c56c728774dd46ebdbb2a
Lucas Mamani <103885226+lucasGME@users.noreply.github.com>
2025-02-13 17:43:39 -0300

    Limpieza

--- funciones.h
+++
@@ -1,7 +0,0 @@
-#ifndef FUNCIONES_H_INCLUDED
-#define FUNCIONES_H_INCLUDED
-
-void Jugar();
-void VerRankingEquipo();
-
-#endif // FUNCIONES_H_INCLUDED
--- menu.h
+++
@@ -1,9 +0,0 @@
-#ifndef MENU_H_INCLUDED
-#define MENU_H_INCLUDED
-#define TRUE 1
-#define FALSE 0
-#include <stdio.h>
-#include <stdlib.h>
-void menu_principal();
-
-#endif // MENU_H_INCLUDED
